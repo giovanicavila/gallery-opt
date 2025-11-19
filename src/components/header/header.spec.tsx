8e7d757a54269dc477d0fd417ed8356f4f56e708
Giovani <giovani.avila.dev@gmail.com>
2025-11-19 11:26:55 -0300

    chore: change to kebab-case header.spec.tsx

---
+++ src/components/header/header.spec.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import { ThemeProvider } from "@/contexts/theme-context";
+import { Header } from "./header";
+
+describe("Header component", () => {
+	it("should render header on the top", () => {
+		render(
+			<ThemeProvider defaultTheme="system" storageKey="test-theme">
+				<Header />
+			</ThemeProvider>
+		);
+
+		const header = screen.getByRole("banner");
+		expect(header).toBeDefined();
+		expect(header.textContent).toContain("The Library");
+	});
+});
+	
